03786f878631894a060063b5b41b8defa9780f9d
AMOviedo15 <AMOviedo15@gmail.com>
2021-02-10 04:52:10 -0600

    xx

--- ZyLabs2-19.py
+++ ZyLabs2-19.py
@@ -1,1 +1,20 @@
+cups_lemon = float(input("Enter amount of lemon juice (in cups):\n"))
+cups_water = float(input("Enter amount of water (in cups):\n"))
+cups_agave = float(input("Enter amount of agave nectar (in cups):\n"))
+servings = float(input("How many servings does this make?\n\n"))
+print("Lemonade ingredients - yields", ('{:.2f}'.format(servings)), "servings")
+print(('{:.2f}'.format(cups_lemon)), "cup(s) lemon juice" )
+print(('{:.2f}'.format(cups_water)), "cup(s) water" )
+print(('{:.2f}'.format(cups_agave)), "cup(s) agave nectar\n" )
 
+total_servings = int(input("How many servings would you like to make?\n\n"))
+scale = total_servings / servings
+print('Lemonade ingredients - yields', ('{:.2f}'.format(total_servings)), 'servings')
+print(('{:.2f}'.format(cups_lemon * scale)), "cup(s) lemon juice")
+print(('{:.2f}'.format(cups_water * scale)), "cup(s) water" )
+print(('{:.2f}'.format(cups_agave * scale)), "cup(s) agave nectar\n" )
+
+print('Lemonade ingredients - yields', ('{:.2f}'.format(total_servings)), 'servings')
+print(('{:.2f}'.format((cups_lemon * scale) / 16)), "gallon(s) lemon juice")
+print(('{:.2f}'.format((cups_water * scale) / 16)), "gallon(s) water" )
+print(('{:.2f}'.format((cups_agave * scale) / 16)), "gallon(s) agave nectar")
